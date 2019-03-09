42933ab7caade36f1a875314d1ad9a67cf69becb
Naookie Sato <necrosato@live.com>
2019-03-09 07:14:14 -0800

    better install script
    :

--- video-security/streaming-server/install_streaming_server.sh
+++ video-security/streaming-server/install_streaming_server.sh
@@ -1,9 +1,30 @@
 #!/bin/bash
+#
+# Run this script to install the rtsp streaming server
+
+set -e
+
+while getopts "hd:" arg; do
+  case $arg in
+    h )
+      echo "-h      : Print this help message."
+      echo "-d DIR  : Give the directory to clone and make the streaming server"
+      ;;
+    d )
+      CLONE_DIR=$OPTARG
+      ;;
+  esac
+done
+
+if [[ "$CLONE_DIR" == "" ]]; then
+  echo "Must pass a directory with -d."
+  exit 1
+fi
 
 CDIR=$(pwd)
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-sudo apt-get install libmoose-perl liburi-perl libmoosex-getopt-perl libsocket6-perl libanyevent-perl ffmpeg
-cd $DIR/rtsp-server
+sudo apt-get install git libmoose-perl liburi-perl libmoosex-getopt-perl libsocket6-perl libanyevent-perl ffmpeg
+git clone https://github.com/revmischa/rtsp-server $CLONE_DIR
+cd $CLONE_DIR
 perl Makefile.PL
 make
 make test
