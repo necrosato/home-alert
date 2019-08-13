cebe8c1d003a412dfb70ff3a1ed521e7916918fa
mn <39255541+margielamadman@users.noreply.github.com>
2019-08-12 21:29:05 -0700

    Update python code to reflect name change

--- home-alert-node/python/node.py
+++ home-alert-node/python/node.py
@@ -3,36 +3,36 @@ import yaml
 
 import home_alert_logging
 from home_alert_camera import HomeAlertCamera
-from home_alert_main_server import HomeAlertMainServer
+from home_alert_web_server import HomeAlertWebServer
 import aws_utils
 
 def main():
-    parser = argparse.ArgumentParser(description='Run the main home alert server.')
+    parser = argparse.ArgumentParser(description='Run the main home alert service.')
     parser.add_argument('-c', '--config', type=str, required=True,
-                         help='Path to a home alert main server config file.')
+                         help='Path to a home alert node config file.')
     args = parser.parse_args()
 
     logger = home_alert_logging.GetHomeAlertLogger()
     logger.info("Loading config file")
     config = yaml.safe_load(open(args.config, 'r'))
-    # TODO: Don't bother pulling these out, pass dict to HomeAlertMainServer
-    location = config['main_server']['location']
+    # TODO: Don't bother pulling these out, pass dict to HomeAlertWebServer
+    location = config['home_alert_node']['location']
 
     logger.info("Creating HomeAlertCamera")
-    video_device = config['main_server']['video_device']
-    video_width = config['main_server']['video_width']
-    video_height = config['main_server']['video_height']
+    video_device = config['home_alert_node']['video_device']
+    video_width = config['home_alert_node']['video_width']
+    video_height = config['home_alert_node']['video_height']
     camera = HomeAlertCamera(video_device, video_width, video_height)
 
     smtp_info = config['smtp_info']
     notify_emails = config['notify_emails']
     s3_bucket = None if 's3_upload_bucket' not in config else config['s3_upload_bucket']
 
-    logger.info("Creating HomeAlertMainServer")
-    home_alert = HomeAlertMainServer(location, smtp_info, camera, notify_emails, s3_bucket)
+    logger.info("Creating HomeAlertWebServer")
+    home_alert = HomeAlertWebServer(location, smtp_info, camera, notify_emails, s3_bucket)
     # Start web server
-    logger.info("Running Main Server")
-    port = config['main_server']['server_port']
+    logger.info("Running Web Server")
+    port = config['home_alert_node']['server_port']
     home_alert.run(port)
 
 
